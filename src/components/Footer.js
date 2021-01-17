2271a7d54d32dbee5508b784e94b3f29a4214845
prasad vallabhaneni <prasadvallabhaneni@prasad-vallabhanenis-MacBook-Air-5.local>
2021-01-17 14:10:09 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+const Footer = () => {
+    return (
+       <footer>
+           <Container>
+             <Row>
+                 <Col className='text-center'>
+                    Copyright &copy; HomeFoods 2020
+                 </Col>
+             </Row>
+           </Container>
+       </footer>
+    )
+}
+
+export default Footer
